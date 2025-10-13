83b3a4fee5ae1f235d24b21be715fc6d3c6fb184
barryibrahima <ibrahimbarryib67@gmail.com>
2025-10-13 10:55:51 +0200

    second commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,27 +6,55 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // Obtenez la session pour suivre les tentatives de connexion
+        $session = $request->getSession();
+        $loginAttempts = $session->get('login_attempts', 0);
+        $lockoutTime = $session->get('lockout_time');
 
-        // get the login error if there is one
+        // Vérifier si le compte est verrouillé
+        if ($loginAttempts >= 3 && $lockoutTime && time() < $lockoutTime) {
+            return $this->render('security/locked.html.twig', [
+                'message' => 'Votre compte est temporairement verrouillé. Veuillez réessayer plus tard.',
+            ]);
+        }
+
+        // Obtenez l'erreur de connexion s'il y en a une
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+        // Dernier nom d'utilisateur saisi par l'utilisateur
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // Si la connexion échoue
+        if ($error) {
+            $session->set('login_attempts', ++$loginAttempts);
+            if ($loginAttempts >= 3) {
+                // Verrouiller le compte pour 1 minute
+                $session->set('lockout_time', time() + 60); // 1 minute
+            }
+        } else {
+            // Réinitialiser après une connexion réussie
+            $session->remove('login_attempts');
+            $session->remove('lockout_time');
+        }
+
+        // Rendre le template de connexion avec les informations nécessaires
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'login_attempts' => $loginAttempts,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        // Cette méthode peut rester vide. Elle sera interceptée par le pare-feu.
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
